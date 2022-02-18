af03f87a9d04f14c67027e62b72a9237cd2456d7
jyb8509 <bestdung8509@gmail.com>
2022-02-18 15:56:07 +0900

    Add: SortTab 현상태 커밋

--- src/UI/Organisms/SortTab/style.SortTab.ts
+++ src/UI/Organisms/SortTab/style.SortTab.ts
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+export const OrderCategoryLists = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-bottom: 14px;
+`
+
+
+export const ChangeSortButton = styled.li`
+  display: list-item;
+  margin-left: 20px;
+  cursor: pointer;
+  color: #999999;
+  font-size: 16px;
+  text-decoration: none;
+  z-index: 20;
+  ${({ active }: any) =>
+    active &&
+    `
+      font-weight: 700;
+      opacity: 1;
+      color: ${({ theme }:any) => theme.fontColor};
+    `}
+`;
