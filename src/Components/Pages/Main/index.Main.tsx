81426fe0f4b80715c37c4b94c0588278ec329cc5
zerochae <zerochae@kakao.com>
2022-02-18 17:26:48 +0900

    Feat: data Sort, Cateogry select 기능 구현

--- src/Components/Pages/Main/index.Main.tsx
+++ src/Components/Pages/Main/index.Main.tsx
@@ -7,35 +7,68 @@ import * as T from "Types/index";
 import SortTab from "Organisms/SortTab/index.SortTab";
 import CategoryTab from "Organisms/CategoryTab/index.CategoryTab";
 
-const Main = () =>  {
+const Main = () => {
   const [JsonData, setJsonData] = useState<T.JsonDataType[]>([]);
-  const [searchInput , setSearchInput] = useState<string>('');
-  const [selectedCategory, setSelectedCategory] = useState<string>('전체');
-  const [selectedSort,setSelectedSort] = useState<string>('인기순');
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("전체");
+  const [selectedSort, setSelectedSort] = useState<string>("인기순");
+  const [filteredData, setFilteredData] = useState<T.JsonDataType[]>([]);
   useEffect(() => {
     (async () => {
       await fetch("http://localhost:4000/results")
         .then((res) => res.json())
-        .then((res) => setJsonData(res));
+        .then((res) => setJsonData(res))
     })();
   }, []);
+
+  useEffect(() => {
+    selectedCategory === "전체"
+      ? setFilteredData(JsonData)
+      : setFilteredData([
+          ...JsonData.filter((el) => el.ingredient === selectedCategory),
+        ]);
+  }, [selectedCategory,JsonData]);
+
+  useEffect(() => {
+    selectedSort === "인기순"
+      ? filteredData.sort((a, b) => b.searchAmount - a.searchAmount)
+      : "낮은 가격순"
+      ? filteredData.sort((a, b) => a.price - b.price)
+      : filteredData.sort((a, b) => b.price - a.price);
+
+      console.log(filteredData)
+  }, [selectedSort]);
 
   return (
     <Container>
       <GlobalStyle />
       <Nav />
-      <SearchBar JsonData={JsonData} searchInput = {searchInput} setSearchInput = {setSearchInput} />
-      <CategoryTab JsonData={JsonData} selectedSort={selectedSort} setSelectedSort={setSelectedSort} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
-      <SortTab selectedSort={selectedSort} setSelectedSort={setSelectedSort} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
+      <SearchBar
+        JsonData={JsonData}
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
+      <CategoryTab
+        JsonData={JsonData}
+        selectedSort={selectedSort}
+        setSelectedSort={setSelectedSort}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <SortTab
+        selectedSort={selectedSort}
+        setSelectedSort={setSelectedSort}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
     </Container>
   );
 };
 
 const Container = styled.div`
-
   width: 60vw;
-  margin:auto;
-  
+  margin: auto;
+
   @media all and (max-width: 768px) {
     width: 100vw;
     margin: auto;
