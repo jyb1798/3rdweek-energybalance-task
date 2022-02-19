e8306094ac7e069a71d70116b724434a790df1c7
jyb8509 <bestdung8509@gmail.com>
2022-02-19 15:02:45 +0900

    Add: Cypress test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,8 +1,21 @@
 describe('start', () => {
-    it('should navigate to the about page', () => {
-      // Start from the index page
-      cy.visit('/')
-  })
+  context('검색', () => {  
+        it('successfully load', () => {
+          cy.visit('https://energybalancee.herokuapp.com/') 
+
+          cy.get('img[alt="로고"]').click();
+    
+          cy.get('div').contains('검색').click()
+    
+          cy.get('button').contains('인기 순').click()
+    
+          cy.get('button').contains('높은가격 순').click()
+    
+          cy.get('button').contains('낮은가격 순').click()
+      })
+    
+     
+    });  
+  });  
 
   
-})
